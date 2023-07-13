3449069e8d2d8514a1d2cd54722f93f92aed3353
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-07-13 18:00:46 -0300

    feat: adicionando client no projeto

---
+++ client/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'prettier/prettier': 'error',
+  },
+};
---
+++ client/.prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  semicolon: true,
+}
+
+/*
+  Arquivo de configuração do Prettier para o servidor.
+  O Prettier é uma ferramenta de formatação de código
+*/

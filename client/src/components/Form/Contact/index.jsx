383ce6152dfd71d26e9bbe3cf0332785e3f3fc2a
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-07-17 16:42:08 -0300

    feat: criando máscara para telefones

--- client/src/components/Form/Contact/index.jsx
+++ client/src/components/Form/Contact/index.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ContactForm, Input, TextArea, SubmitButton } from './styles';
+import formatPhone from '../../../util/formatPhone';
 
 export default function Contact() {
+  const [phone, setPhone] = useState('');
+
+  const handlePhoneChange = (event) => {
+    event.preventDefault();
+    setPhone(formatPhone(event.target.value));
+  };
+
   return (
-    <ContactForm className="section-contact__form">
+    <ContactForm>
+      <Input type="text" placeholder="Nome" />
+      <Input type="email" placeholder="E-mail" />
       <Input
-        className="section-contact__input"
-        type="text"
-        placeholder="Nome"
-      />
-      <Input
-        className="section-contact__input"
-        type="email"
-        placeholder="E-mail"
-      />
-      <Input
-        className="section-contact__input"
         type="text"
         placeholder="Telefone"
+        value={phone}
+        onChange={handlePhoneChange}
       />
       <TextArea
         className="section-contact__textarea"

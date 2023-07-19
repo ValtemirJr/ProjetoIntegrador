9bb584aebdba4e7c4e63a726bfcac9e61d1110cf
Valtemir Junior <100030801+ValtemirJr@users.noreply.github.com>
2023-07-19 00:56:52 -0300

    Create 20230719024037-scheduling.js

---
+++ server/src/database/migrations/20230719024037-scheduling.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('scheduling', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    client_id: {
+      type: Sequelize.INTEGER,
+      references: { model: 'client', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      allowNull: false,
+    },
+    service_id: {
+      type: Sequelize.INTEGER,
+      references: { model: 'service', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      allowNull: false,
+    },
+    date_request: {
+      allowNull: false,
+      type: Sequelize.STRING(255),
+    },
+    consultation_date: {
+      allowNull: false,
+      type: Sequelize.STRING(255),
+    },
+    goal: {
+      allowNull: false,
+      type: Sequelize.STRING(255),
+    },
+    status: {
+      allowNull: false,
+      type: Sequelize.BOOLEAN,
+    },
+  }),
+
+  down: (queryInterface) => queryInterface.dropTable('scheduling'),
+};
